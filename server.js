cf2cd912e6c72d43db2c7500ff60edb8086f9e9e
igot2bfreee <igot2bfreee@gmail.com>
2020-10-17 12:09:56 -0700

    workshopWeek1Task3

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,6 +15,7 @@ app.use(bodyParser.json());
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 
 app.use(express.static(__dirname + '/public'));
